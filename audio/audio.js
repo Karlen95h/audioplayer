3c5bd12288811fc671d2d5c87bf1e19b92cf972f
Karlen95h <karlen.h.1995@gmail.com>
2022-03-06 15:53:46 +0400

    audio

---
+++ audio/audio.js
@@ -0,0 +1,19 @@
+var data = {
+    title:[
+        "Gyumri",
+        "Komitas_Shushiki",
+        "Rey_Charles_Jack"
+    ],
+    song:[
+        "music/Gyumri.mp3",
+        "music/Komitas_Shushiki.mp3",
+        "music/Rey_Charles_Jack.mp3"
+
+    ],
+    poster:[
+        "https://c.tenor.com/d_IO8M1rCD0AAAAC/listening-to-music-jerry.gif",
+        "https://c.tenor.com/lhlDEs5fNNEAAAAC/music-beat.gif",
+        "https://thumbs.gfycat.com/EdibleCriminalAntarcticgiantpetrel-size_restricted.gif"
+
+    ]
+}
