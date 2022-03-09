61c994c9a6177d5d4b46b3b951eec12de9fd89ec
Karlen95h <karlen.h.1995@gmail.com>
2022-03-09 13:53:35 +0400

    audio js

--- audio/audio.js
+++ audio/audio.js
@@ -42,10 +42,74 @@ function playSong(){
     let songTitle = document.getElementById("songTitle");
     songTitle.textContent = data.title[curentSong];//???
     let img = document.getElementById('row1');
-    img.style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+    img.style.backgroundImage = "url(" + data.poster[currentSong] + ")";//+ ik neri syntax ?
     let main  = document.getElementById('main')
     main.style.backgroundImage = "url("+  data.poster[curentSong] + ")";
 
 }
 
 
+function playOrPauseSong(){
+    let play = document.getElementById("play")
+    // console.log(play);
+    if(song.paused){//paused?
+            song.play()
+        play.src = "https://i.pinimg.com/474x/cb/f1/ba/cbf1ba6cba8053055f09d9b77fe2b884.jpg"
+
+    }else{
+        song.pause();
+        play.src = "https://w7.pngwing.com/pngs/635/67/png-transparent-control-music-pause-play-stop-music-player-controls-icon-thumbnail.png"
+    }
+}
+
+song.addEventListener("timeupdate",function(){
+    //  console.log(song.currentTime);
+   // console.log(song.duration);???
+   let fill = document.getElementById("fill")
+
+   let position = song.currentTime / song.duration;
+   fill.style.width = position * 100 + "%";//fill
+
+   convertTime(song.currentTime)//current Time
+   if (song.ended) {
+       next()
+   }
+})
+
+
+function convertTime(seconds){
+    let currentTime = document.getElementById("currentTime")
+    let min = Math.floor(seconds/60)
+    let sec = Math.floor(seconds%60)//?
+    min = (min < 10 ) ? "0" + min : min;
+    sec = (min < 10 ) ? "0" + sec : sec;
+
+    currentTime.textContent = min + ":" + sec;
+    totalTime(Math.round(song.duration))
+
+
+}
+
+function next(){
+    curentSong++;
+    if ((currentSong >= data.song.length)) {
+        curentSong = 0
+    }
+    playSong();
+    song.play();
+    play.src = "https://png.pngtree.com/element_our/20190530/ourmid/pngtree-cartoon-next-song-icon-download-image_1256944.jpg"
+
+
+}
+
+function prev(){
+    curentSong--;
+    if(curentSong < 0){
+        curentSong = data.song.length - 1;
+
+    }
+    playSong();
+    song.play();
+    play.src = "https://i.pinimg.com/474x/cb/f1/ba/cbf1ba6cba8053055f09d9b77fe2b884.jpg";
+    
+}
